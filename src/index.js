81c5cc595840f38e52ff4d513cccc4dc812dcbdc
Neveod <neve@ravenbay.com>
2023-11-26 21:02:25 -0700

    fixed src typo

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function refreshWeather(response) {
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
-  icon.innerHTML = `<img scr="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
